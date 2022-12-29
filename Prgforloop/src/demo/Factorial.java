a3b78a243f013e7d1f9061254f44a159213707a8
MANU <MANU@NANDHIUMA>
2022-12-29 14:11:46 +0530

    Build B011

---
+++ Prgforloop/src/demo/Factorial.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Factorial
+{
+	public static void main(String[] args) {
+		int n=6;
+		int fact=1;
+		for(int i=n;i<=10;i++)
+		{
+			fact=fact*i;
+			System.out.println(fact);
+		}
+		
+	} 
+
+}
